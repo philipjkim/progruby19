7b9d9410bb80254f6f781a167358bba0ab6cfc31
Soo Philip Kim <philipjkim@gmail.com>
2012-05-17 23:52:58 +0900

    added attr_reader instead of accessor methods

--- ch03/tutclasses.rb
+++ ch03/tutclasses.rb
@@ -1,16 +1,11 @@
 class BookInStock
+  attr_reader :isbn, :price
   def initialize(isbn, price)
     @isbn = isbn
     @price = Float(price)
   end
   def to_s
     "ISBN: #{@isbn}, price: #{@price}"
-  end
-  def isbn
-    @isbn
-  end
-  def price
-    @price
   end
 end
 
