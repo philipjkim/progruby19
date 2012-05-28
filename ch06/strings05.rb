bd8a9b2db6199d9bd10e32ee71c6d0ed51b1ce09
Soo Philip Kim <philipjkim@gmail.com>
2012-05-28 23:38:52 +0900

    converted mm:ss formatted string to seconds

--- ch06/strings05.rb
+++ ch06/strings05.rb
@@ -4,7 +4,8 @@ File.open("songdata") do |song_file|
   song_file.each do |line|
     file, length, name, title = line.chomp.split(/\s*\|\s*/)
     name.squeeze!(" ")
-    songs << Song.new(title, name, length)
+    mins, secs = length.scan(/\d+/)
+    songs << Song.new(title, name, mins.to_i*60 + secs.to_i)
   end
   puts songs
 end
