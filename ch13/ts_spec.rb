064517853ad01a831353e462bae2d44b56f781f7
Soo Philip Kim <philipjkim@gmail.com>
2012-06-07 19:15:37 +0900

    added specs for TennisScorer

---
+++ ch13/ts_spec.rb
@@ -0,0 +1,6 @@
+describe "TennisScorer", "basic scoring" do
+  it "should start with a score of 0-0"
+  it "should be 15-0 if the server wins a point"
+  it "should be 0-15 if the receiver wins a point"
+  it "should be 15-15 after they both win a point"
+end
