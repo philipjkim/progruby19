d715031540a8638c222a97a8967bf90a9c7ec184
Soo Kim <sooo@daumcorp.com>
2012-06-07 15:51:37 +0900

    added refactored test for Roman

---
+++ ch13/unit_testing_03.rb
@@ -0,0 +1,13 @@
+require_relative 'roman'
+require 'test/unit'
+class TestRoman < MiniTest::Unit::TestCase
+  NUMBERS = [[1, "i"], [2, "ii"], [3, "iii"],
+             [4, "iv"], [5, "v"], [9, "ix"]
+  ]
+  def test_simple
+    NUMBERS.each do |arabic, roman|
+      r = Roman.new(arabic)
+      assert_equal roman, r.to_s
+    end
+  end
+end
