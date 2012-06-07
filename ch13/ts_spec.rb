32d5e0ce5c0732f06ce54fd0671dec279fc695c9
Soo Philip Kim <philipjkim@gmail.com>
2012-06-07 19:26:39 +0900

    modified spec to pass 2 other tests

--- ch13/ts_spec.rb
+++ ch13/ts_spec.rb
@@ -1,14 +1,22 @@
 require_relative "tennis_scorer"
 describe "TennisScorer", "basic scoring" do
+  before(:each) do
+    @ts = TennisScorer.new
+  end
   it "should start with a score of 0-0" do
-    ts = TennisScorer.new
-    ts.score.should == "0-0"
+    @ts.score.should == "0-0"
   end
   it "should be 15-0 if the server wins a point" do
-    ts = TennisScorer.new
-    ts.give_point_to(:server)
-    ts.score.should == "15-0"
+    @ts.give_point_to(:server)
+    @ts.score.should == "15-0"
   end
-  it "should be 0-15 if the receiver wins a point"
-  it "should be 15-15 after they both win a point"
+  it "should be 0-15 if the receiver wins a point" do
+    @ts.give_point_to(:receiver)
+    @ts.score.should == "0-15"
+  end
+  it "should be 15-15 after they both win a point" do
+    @ts.give_point_to(:server)
+    @ts.give_point_to(:receiver)
+    @ts.score.should == "15-15"
+  end
 end
