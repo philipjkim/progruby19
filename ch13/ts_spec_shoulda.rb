edc36a548d7fee8ae90130b31b3bb52694559210
Soo Philip Kim <philipjkim@gmail.com>
2012-06-07 20:13:48 +0900

    completed chapter 13

--- ch13/ts_spec_shoulda.rb
+++ ch13/ts_spec_shoulda.rb
@@ -12,18 +12,25 @@ class TennisScorerTest < Test::Unit::TestCase
     should "start with a score of 0-0" do
       assert_score "0-0"
     end
-    should "be 15-0 if the server wins a point" do
-      @ts.give_point_to(:server)
-      assert_score "15-0"
+    context "where the server wins a point" do
+      setup do
+        @ts.give_point_to(:server)
+      end
+      should "be 15-0" do
+        assert_score "15-0"
+      end
+      context "and the opponent wins a point" do
+        setup do
+          @ts.give_point_to(:receiver)
+        end
+        should "be 15-15" do
+          assert_score "15-15"
+        end
+      end
     end
     should "be 0-15 if the receiver wins a point" do
       @ts.give_point_to(:receiver)
       assert_score "0-15"
-    end
-    should "be 15-15 if the receiver wins a point" do
-      @ts.give_point_to(:server)
-      @ts.give_point_to(:receiver)
-      assert_score "15-15"
     end
   end
 end
