a5c289f4fc609a1f3585b61cae916efdfc0417a4
Soo Philip Kim <philipjkim@gmail.com>
2012-06-07 19:23:28 +0900

    modified spec and TennisScorer to pass the 2nd test

--- ch13/tennis_scorer.rb
+++ ch13/tennis_scorer.rb
@@ -1,5 +1,17 @@
 class TennisScorer
+  OPPOSITE_SIDE_OF_NET = {
+      :server => :receiver,
+      :receiver => :server
+  }
+  def initialize
+    @score = { :server => 0, :receiver => 0 }
+  end
   def score
-    "0-0"
+    "#{@score[:server]*15}-#{@score[:receiver]*15}"
+  end
+  def give_point_to(player)
+    other = OPPOSITE_SIDE_OF_NET[player]
+    fail "Unknown player #{player}" unless other
+    @score[player] += 1
   end
 end
--- ch13/ts_spec.rb
+++ ch13/ts_spec.rb
@@ -4,7 +4,11 @@ describe "TennisScorer", "basic scoring" do
     ts = TennisScorer.new
     ts.score.should == "0-0"
   end
-  it "should be 15-0 if the server wins a point"
+  it "should be 15-0 if the server wins a point" do
+    ts = TennisScorer.new
+    ts.give_point_to(:server)
+    ts.score.should == "15-0"
+  end
   it "should be 0-15 if the receiver wins a point"
   it "should be 15-15 after they both win a point"
 end
