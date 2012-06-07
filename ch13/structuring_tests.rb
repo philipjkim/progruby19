a6cd39ad46488fdd754ff4cef961bd798aaed18e
Soo Kim <sooo@daumcorp.com>
2012-06-07 17:29:23 +0900

    refactored test using setup and teardown

--- ch13/structuring_tests.rb
+++ ch13/structuring_tests.rb
@@ -2,30 +2,28 @@ require 'test/unit'
 require 'dbi'
 require 'playlist_builder'
 class TestPlaylistBuilder < Test::Unit::TestCase
+  def setup
+    @db = DBI.connect('DBI:mysql:playlists')
+    @pb = PlaylistBuilder.new(@db)
+  end
+  def teardown
+    @db.disconnect
+  end
   def test_empty_playlist
-    db = DBI.connect('DBI:mysql:playlists')
-    pb = PlaylistBuilder.new(db)
-    assert_empty pb.playlist
-    db.disconnect
+    assert_empty @pb.playlist
   end
   def test_artist_playlist
-    db = DBI.connect('DBI:mysql:playlists')
-    pb = PlaylistBuilder.new(db)
-    pb.include_artist "krauss"
-    refute_empty pb.playlist, "Playlist shouldn't be empty"
-    pb.playlist.each do |entry|
+    @pb.include_artist "krauss"
+    refute_empty @pb.playlist, "Playlist shouldn't be empty"
+    @pb.playlist.each do |entry|
       assert_match /krauss/i, entry.artist
     end
-    db.disconnect
   end
   def test_title_playlist
-    db = DBI.connect('DBI:mysql:playlists')
-    pb = PlaylistBuilder.new(db)
-    pb.include_title "midnight"
-    refute_empty pb.playlist, "Playlist shouldn't be empty"
-    pb.playlist.each do |entry|
+    @pb.include_title "midnight"
+    refute_empty @pb.playlist, "Playlist shouldn't be empty"
+    @pb.playlist.each do |entry|
       assert_match /krauss/i, entry.title
     end
-    db.disconnect
   end
 end
